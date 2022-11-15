58a17446f1940f289103089937be9150ffb43d05
madhu <madhu.java.git@gmail.com>
2022-11-15 16:48:30 +0530

    commit

--- StringApp/src/String1.java
+++ StringApp/src/String1.java
@@ -5,7 +5,7 @@ public class String1 {
 		String s = "Sachin";
 		System.out.println(s.length());
 		s=s.replace(" ", "");
-		s=s.replace(",", "");
+		s=s.replace(", ", "");
 		System.out.println(s.toLowerCase());
 
 	}
